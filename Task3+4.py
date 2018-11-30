f7adcefdcd2fc81ffd5188e6afdf837450c54b95
Zakariyya Ahmed <ahmedz27@uni.coventry.ac.uk>
2018-11-30 01:38:59 +0000

    Task 4 isConnected Implementation

--- Task3+4.py
+++ Task3+4.py
@@ -40,10 +40,33 @@ class Graph():
                 for lst3 in self.list.get(lst2).edge:
                     lst.append(lst3)
         return visited
+    
+    
+    def DFS_Code(self, v):
+        lst = []
+        visited = []
+        lst.append(v)
+        while len(lst) > 0:
+            lst2 = lst.pop()
+            if lst2 not in visited:
+                visited.append(lst2)
+                for lst3 in self.list.get(lst2).edge:
+                    lst.append(lst3)
+        printtxt = open("Task5Node.txt", "a+")
+        printtxt.write("\n" "This is the Path traversed for the Depth-First Search" "\n")
+        printtxt.write(str(visited))
+        printtxt.close()
+        return visited
+    
+#Task 4 isConnected Implementation
 
-
-
-
+    def isConnected(self):
+        if len(self.DFS_Code (0)) == len(self.list):
+            print("Yes")
+        else:
+            print("No")
+#-----------------------------------------------------#
+            
 G = Graph()
 G.add_vertex(0)
 G.add_vertex(1)
@@ -62,3 +85,4 @@ G.add_edge(3,1)
 G.add_edge(4,3)
 G.add_edge(3,5)
 print(G.isPath(3,0))
+G.isConnected()
